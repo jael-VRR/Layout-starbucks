d65aeee8dacdb1ca8a5561ed36d6330843440443
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-13 12:10:07 -0500

    new route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {
   Switch,
   Route,
 } from "react-router-dom"
-import grid from "./layout/grid";
+import Grid from "./layout/Grid.jsx";
+import Selector1 from './layout/Selector.jsx';
 
 
 
@@ -14,7 +15,10 @@ function App() {
   return (
     <Router>
         <Switch>
-          <Route exact path="/" component={grid} />
+          <Route exact path="/" component={Grid} />
+        </Switch>
+        <Switch>
+          <Route exact path="/selector1" component={Selector1} />
         </Switch>
     </Router>
   );
